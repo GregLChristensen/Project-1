043365e86a66cf1f5ea7e9e0e25e4ac08e32de34
TonyBee <josebarajas2169@gmail.com>
2018-12-17 19:11:30 -0700

    styled

--- assets/app.js
+++ assets/app.js
@@ -14,46 +14,46 @@ var database = firebase.database();
 
 
 
-// Variable with Pet Finder API url---- This can be changed just a place holder
-var queryURL = 'http://api.petfinder.com/my.method?format=json&key=12345&callback=?';
+// // Variable with Pet Finder API url---- This can be changed just a place holder
+// var queryURL = 'http://api.petfinder.com/my.method?format=json&key=12345&callback=?';
  
-// Ajax call
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function(response) {
+// // Ajax call
+// $.ajax({
+//     url: queryURL,
+//     method: "GET"
+// }).then(function(response) {
 
-});
+// });
 
 
 
-function loadgif () {
+// function loadgif () {
 
-	// Storing our giphy API URL for a random cat or dog image
-	var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=b3pwT2Dgw57RUVbsOOWFaiNJH5z0XGjM&tag=cats+dogs";
+// 	// Storing our giphy API URL for a random cat or dog image
+// 	var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=b3pwT2Dgw57RUVbsOOWFaiNJH5z0XGjM&tag=cats+dogs";
 
-	// Perfoming an AJAX GET request to our queryURL
-	$.ajax({
-	  url: queryURL,
-	  method: "GET"
-	})
+// 	// Perfoming an AJAX GET request to our queryURL
+// 	$.ajax({
+// 	  url: queryURL,
+// 	  method: "GET"
+// 	})
 
-	// After the data from the AJAX request comes back
-	  .then(function(response) {
+// 	// After the data from the AJAX request comes back
+// 	  .then(function(response) {
 
-	  // Saving the image_original_url property
-		var imageUrl = response.data.image_original_url;
+// 	  // Saving the image_original_url property
+// 		var imageUrl = response.data.image_original_url;
 
-		// Creating and storing an image tag
-		var catDogImage = $("<img>");
+// 		// Creating and storing an image tag
+// 		var catDogImage = $("<img>");
 
-		// Setting the catDogImage src attribute to imageUrl
-		catDogImage.attr("src", imageUrl);
-		catDogImage.attr("alt", "cat+dog image");
+// 		// Setting the catDogImage src attribute to imageUrl
+// 		catDogImage.attr("src", imageUrl);
+// 		catDogImage.attr("alt", "cat+dog image");
 
-		// Prepending the catDogImage to the images div
-		$("#images").prepend(catDogImage);
-	  });
-  };
-  window.onload = loadgif;
-  console.log(loadgif)
+// 		// Prepending the catDogImage to the images div
+// 		$("#images").prepend(catDogImage);
+// 	  });
+//   };
+//   window.onload = loadgif;
+//   console.log(loadgif)
